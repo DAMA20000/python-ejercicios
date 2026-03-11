22fa5aa7e40aa43f69bbeede0120c5952636383b
Dama Paz <161375416+DAMA20000@users.noreply.github.com>
2026-03-10 23:17:22 -0300

    Add files via upload

---
+++ ejercicios.py
@@ -0,0 +1,29 @@
+# Funcion que calcula el volumen de una esfera
+
+def volumen_esfera(radio):
+    pi = 3.1416
+    volumen = (4/3) * pi * (radio ** 3)
+    return volumen
+
+print(volumen_esfera(5))
+
+# Variables Basicas 
+
+nombre = "Damaris"
+
+edad = 25
+
+ciudad = "Jujuy"
+
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Ciudad:", ciudad)
+
+#operaciones con variables
+a = 10
+b = 6
+
+suma = a + b
+multiplicacion = a * b
+print("Suma:", suma)
+print("Multiplicacion:", multiplicacion)
