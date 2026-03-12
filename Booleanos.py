36f9b339a21918955fe8369a41843d05d3f93cf9
Dama Paz <161375416+DAMA20000@users.noreply.github.com>
2026-03-12 18:18:48 -0300

    Bool
    En Python, los booleanos son un tipo de dato que solo puede tener dos valores posibles:
    
    True → verdadero
    
    False → falso
    
    Se usan para tomar decisiones en el código, especialmente en condicionales (if), comparaciones y lógica.

---
+++ Booleanos.py
@@ -0,0 +1,34 @@
+# en Python, los booleanos son un tipo de dato que solo puede tenr dos valores posibles
+# True (verdadero) y False (falso). Se utilizan para representar la verdad o falsedad de una afirmación o condición.
+# un bolealo responde a una pregunta o afirmación, y se utiliza en 
+# estructuras de control de flujo como condicionales (if) y bucles (while) para tomar decisiones basadas en ciertas condiciones.
+
+booleano = False
+print(booleano)
+booleano = True
+print(booleano)
+
+True
+False
+
+es_mayor = True
+tiene_permiso = False
+
+print(es_mayor)
+print(tiene_permiso)
+
+# los booleanos también se pueden obtener a partir de comparaciones o expresiones lógicas
+edad = 20   
+
+edad = 20
+
+if edad >= 18:
+    print("Eres mayor de edad")
+
+    edad = 20
+tiene_dni = True
+
+print(edad >= 18 and tiene_dni)
+
+print(10 > 5 or 3 > 10)
+  
